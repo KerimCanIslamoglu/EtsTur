542a6f77a979ef6cd4bf358de0270142ddc1a560
Kerim <kerimcanislamoglu1@gmail.com>
2021-01-23 21:32:15 +0300

    added validation

--- Etstur.API/Etstur.WEB/Controllers/HomeController.cs
+++ Etstur.API/Etstur.WEB/Controllers/HomeController.cs
@@ -15,13 +15,7 @@ namespace Etstur.WEB.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             var model = new ResponseListModel<UserModel>();
@@ -57,33 +51,35 @@ namespace Etstur.WEB.Controllers
         [HttpPost]
         public IActionResult CreateUser(UserModel userModel)
         {
-            var model = new ResponseListModel<UserModel>();
-            var client = new HttpClient();
-            try
+            if (ModelState.IsValid)
             {
-                var json = JsonConvert.SerializeObject(userModel);
-                var data = new StringContent(json, Encoding.UTF8, "application/json");
-
-                string baseAddress = "https://localhost:44337/api/User/CreateUser";
-
-                using (HttpResponseMessage httpResponse = client.PostAsync(baseAddress, data).Result)
+                var model = new ResponseListModel<UserModel>();
+                var client = new HttpClient();
+                try
                 {
-                    httpResponse.EnsureSuccessStatusCode();
-                    model = JsonConvert.DeserializeObject<ResponseListModel<UserModel>>(httpResponse.Content.ReadAsStringAsync().Result);
+                    var json = JsonConvert.SerializeObject(userModel);
+                    var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    string baseAddress = "https://localhost:44337/api/User/CreateUser";
+
+                    using (HttpResponseMessage httpResponse = client.PostAsync(baseAddress, data).Result)
+                    {
+                        httpResponse.EnsureSuccessStatusCode();
+                        model = JsonConvert.DeserializeObject<ResponseListModel<UserModel>>(httpResponse.Content.ReadAsStringAsync().Result);
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                model = new ResponseListModel<UserModel>
+                catch (Exception ex)
                 {
-                    Success = false,
-                    Message = ex.ToString(),
-                    Response = null
-                };
+                    model = new ResponseListModel<UserModel>
+                    {
+                        Success = false,
+                        Message = ex.ToString(),
+                        Response = null
+                    };
+                }
+                return RedirectToAction("Index", model);
             }
-
-
-            return RedirectToAction("Index", model);
+            return View();
         }
     }
 }
--- Etstur.API/Etstur.WEB/Models/UserModel.cs
+++ Etstur.API/Etstur.WEB/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,15 @@ namespace Etstur.WEB.Models
     public class UserModel
     {
         public int? Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string BloodType { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
         public string Address { get; set; }
     }
 }
